38b1dd3df7eeaaae829c0edb725afef4c0e9435e
michael simpson <scannermonkey@gmail.com>
2022-07-21 13:37:23 +1000

    C-M-H-s keys

---
+++ keys.el
@@ -0,0 +1,5 @@
+;; set keys for Apple keyboard, for emacs in OS X
+(setq mac-command-modifier 'super) ; make cmd key do Meta
+(setq mac-option-modifier 'meta) ; make opt key do Super
+(setq mac-control-modifier 'control) ; make Control key do Control
+;;(setq ns-function-modifier 'hyper)  ; make Fn key do Hyper
